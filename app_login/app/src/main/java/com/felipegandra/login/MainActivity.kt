2c1f18812c9feb1f5232e954034892c51317d72a
fgandraf <fgandraf@gmail.com>
2024-02-22 20:22:09 -0300

    feat: Add Login (Internationalization) Project

---
+++ app_login/app/src/main/java/com/felipegandra/login/MainActivity.kt
@@ -0,0 +1,166 @@
+package com.felipegandra.login
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.felipegandra.login.ui.theme.LoginTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Login()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Login() {
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    var erroEmail by remember {
+        mutableStateOf(false)
+    }
+
+    var erroSenha by remember {
+        mutableStateOf(false)
+    }
+
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(id = R.string.login),
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Blue
+        )
+        Text(text = stringResource(id = R.string.subtitle))
+        Spacer(modifier = Modifier.height(48.dp))
+        Card(modifier = Modifier
+            .fillMaxWidth()) {
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(32.dp)) {
+
+
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    modifier = Modifier.fillMaxWidth(),
+                    label = {
+                        Text(text = stringResource(id = R.string.email))
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email
+                    ),
+                    isError = erroEmail
+                )
+
+
+                if (erroEmail){
+                    Text(
+                        text = stringResource(id = R.string.email_required),
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.End,
+                        fontSize = 12.sp,
+                        color = Color.Red,
+                        fontStyle = FontStyle.Italic
+                    )
+                }
+
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = {
+                        if (it.length <= 8) password = it
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    label = {
+                        Text(text = stringResource(id = R.string.password))
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password
+                    ),
+                    visualTransformation = PasswordVisualTransformation(),
+                    isError = erroSenha
+                )
+
+                if (erroSenha){
+                    Text(
+                        text = stringResource(id = R.string.password_required),
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.End,
+                        fontSize = 12.sp,
+                        color = Color.Red,
+                        fontStyle = FontStyle.Italic
+                    )
+                }
+
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+
+                Button(onClick = {
+                    erroEmail = email.isEmpty()
+                    erroSenha = password.isEmpty()
+                }) {
+                    Text(
+                        text = stringResource(id = R.string.enter),
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+
+            }
+        }
+    }
+}
