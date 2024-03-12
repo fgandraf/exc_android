0c7c9069b8b37ef0163d5b17a518415d1df2d786
fgandraf <fgandraf@gmail.com>
2024-03-11 22:55:22 -0300

    feat: Add Lazy Row

--- app_lazy/app/src/main/java/com/felipegandra/lazy/MainActivity.kt
+++ app_lazy/app/src/main/java/com/felipegandra/lazy/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.felipegandra.lazy
 
-import androidx.compose.ui.tooling.preview.Preview
-import com.felipegandra.lazy.ui.theme.LazyTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -23,11 +25,20 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.felipegandra.lazy.repository.getAllGames
+import com.felipegandra.lazy.model.Game
+import com.felipegandra.lazy.repository.getGamesByStudio
+import com.felipegandra.lazy.ui.theme.LazyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,9 +56,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GamesScreen() {
+
+    var studioState by remember { mutableStateOf("") }
+    var listGamesByStudio by remember { mutableStateOf(getGamesByStudio(studioState)) }
+
+
     Column(modifier = Modifier.padding(16.dp)) {
         Text(
             text = "Meus jogos favoritos",
@@ -56,14 +71,19 @@ fun GamesScreen() {
         )
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = studioState,
+            onValueChange = {
+                studioState = it
+                listGamesByStudio = getGamesByStudio(studioState)
+                },
             modifier = Modifier.fillMaxWidth(),
             label = {
                 Text(text = "Nome do estúdio")
             },
             trailingIcon = {
-                IconButton(onClick = {  }) {
+                IconButton(onClick = {
+                    listGamesByStudio = getGamesByStudio(studioState)
+                }) {
                     Icon(
                         imageVector = Icons.Default.Search,
                         contentDescription = ""
@@ -72,20 +92,75 @@ fun GamesScreen() {
             }
         )
         Spacer(modifier = Modifier.height(16.dp))
-        LazyColumn(){
-//            item {
-//                Text(text = "Item 1")
-//            }
-            items(getAllGames()){
-                Column() {
-                    Text(text = it.title)
-                }
+
+        LazyRow{
+            items(listGamesByStudio){
+                StudioCard(game = it)
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn{
+            items(listGamesByStudio){
+                GameCard(game = it)
             }
 
         }
     }
 }
 
+
+@Composable
+fun StudioCard(game: Game) {
+    Card(modifier = Modifier.size(100.dp).padding(end = 4.dp)) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(text = game.studio)
+        }
+    }
+}
+
+@Composable
+fun GameCard(game: Game) {
+    Card(modifier = Modifier.padding(bottom = 8.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .weight(3f)) {
+                Text(
+                    text = game.title,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+
+
+                    text = game.studio,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal
+                )
+            }
+            Text(
+                text = game.releaseYear.toString(),
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue
+            )
+        }
+    }
+}
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GamesScreenPreview() {
